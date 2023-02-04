c62071974a8215407757f4dcfc3e8aee3d9c3e2a
rochoa13 <ochoarafael.jr@gmail.com>
2023-02-04 04:50:47 +0000

    Complete Boolean

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,12 +14,13 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        if( b == true){
+        /*if( b == true){
             return true;
         }
         else{
         return false;
-        }
+        }*/
+        return b;
     }
 
     /**
